f1042d5cc2b830bb81d84cb7ca8604acc326fadd
DavidYu1010 <provic0099@gmail.com>
2022-02-06 23:16:44 +0800

    limit calling approve() method for only proposalContracts, add new method to set proposal contract address

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -6,13 +6,18 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 contract Dao is Ownable{
     uint256 public voteFee;
     IERC20 governanceToken;
+    address createTokenProposal;
     constructor(address govAddr) {
         governanceToken = IERC20(govAddr);
     }
     function setVoteFee(uint256 fee_) public onlyOwner{
         voteFee = fee_;
     }
+    function setProposalAddress(address addr_) public onlyOwner{
+        createTokenProposal = addr_;
+    }
     function approve(address spender, uint256 amount) public {
+        require(msg.sender == createTokenProposal, 'Only porposal contracts can approve DAO wallet.');
         governanceToken.approve(spender, amount);
     }
 }
